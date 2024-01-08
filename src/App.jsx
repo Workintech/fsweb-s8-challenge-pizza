73d9f0da9f105e48ba02749b8be8579f87d42e44
Gökhan <gn.ozdemir@gmail.com>
2024-01-08 20:13:58 +0300

    sharable min

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import workintech from '/workintech.svg'
 import './App.css'
 
 function App() {
@@ -9,24 +9,24 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
+          <img src={workintech} className="logo" alt="Workintech logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Workintech + 🍕</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Absolute Acı Pizza sayısı {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Workintech or Pizza logos to learn more
       </p>
     </>
   )
